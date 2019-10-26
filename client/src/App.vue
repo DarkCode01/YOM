35e7e42c0b232827392289b6334968b2f95c3fb6
Rafael Mejia <devrafx@gmail.com>
2019-10-26 14:07:52 -0400

    Vista principal preparada

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,100 @@
 <template>
-  <v-app>
-    <v-app-bar app>
+  <v-app id="keep">
+    <v-app-bar
+      app
+      clipped-left
+      color="red darken-4"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="search"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <span class="title ml-3 mr-5">YOM</span>
     </v-app-bar>
 
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      color="grey lighten-4"
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template>
+          <div>
+            <h1>Hola</h1>
+          </div>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <h1>Hello</h1>
+      <v-container
+        fluid
+        class="grey lighten-4 fill-height"
+      >
+        <v-row
+          justify="center"
+          align="center"
+        >
+          <v-col class="shrink">
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  :href="source"
+                  icon
+                  large
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  href="https://codepen.io/johnjleider/pen/zgxbYO"
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-codepen</v-icon>
+                </v-btn>
+              </template>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
 
-export default {
-  name: 'App'
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: false
+    }),
+  }
 </script>
+
+<style>
+#keep .v-navigation-drawer__border {
+  display: none
+}
+</style>
