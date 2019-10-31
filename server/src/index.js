3f7213519b3354057827f255304da014547da273
Lea-Reif <32004646+Lea-Reif@users.noreply.github.com>
2019-10-31 11:00:11 -0400

    Init

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express'),
     morgan = require('morgan'),
     cors = require('cors'),
     router = require('./routes/routes'),
+    bodyParser= require('body-parser'),
     port = process.env.PORT || 1234,
     app = express();
 
@@ -9,7 +10,11 @@ require('./db');
 
 app //Referencia al objeto express, solo instanciar a partir de aqui las demas funciones en cadena.
     .use(cors()) //middleware para evitar errores de cors en produccion
+    
+    .use(bodyParser.urlencoded({extended:true})) //para validacion de formularios en formato app/x-www-form-urlencoded
 
+    .use(bodyParser.json()) //para validacion de formularios en formato app/JSON
+    
     .use(morgan('dev')) //middleware para seguir el rastro de las peticiones HTTP por consola
 
     .use('/', router) //Creacion de las rutas externas
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express'),
     router = Router(),
     { usuario, articulo } = require('../models/modelos');
-
+router.get('/', (req,res)=>{
+    res.json({ 'message': 'Welcome to firstpage, you DH'});
+})
 router.get('/welcome', (req, res) => {
     res.send("Hello!");
 })
