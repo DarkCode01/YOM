57488e532ad8441eb468a58f2f2650908aa6ee34
Rafael Mejia <devrafx@gmail.com>
2019-11-02 22:05:37 -0400

    Componente navbar creado.

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,68 @@
+<template>
+    <nav>
+        <v-app-bar
+      app
+      clipped-left
+      color="red darken-4"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="search"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <div>
+        <router-link to="/">
+          <v-btn flat text>
+            <span class="title ml-3 mr-5">YOM</span>
+          </v-btn>
+        </router-link>
+      </div>
+      <div class="my-2 hidden-md-and-down">
+        <router-link to="/">
+          <v-btn small text>Sign in</v-btn>
+        </router-link>
+        <router-link to="/login">
+          <v-btn small text>Log in</v-btn>
+        </router-link>
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      color="grey lighten-4"
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+          <div>
+            <router-link to="/">Go to Home</router-link>
+            <router-link to="/about">Go to About</router-link>
+          </div>
+      </v-list>
+    </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+    export default {
+    name: "navbar",
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: false
+    }),
+  }
+</script>
+
+<style scoped>
+
+</style>
