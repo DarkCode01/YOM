2327fe00dd42c07339c3d613765058d5435ac07e
Rafael Mejia <devrafx@gmail.com>
2019-11-04 14:23:29 -0400

    Card Product obtiene sus datos del store cuando la base de datos esta encendida y con datos almacenados

--- client/src/components/CardProduct.vue
+++ client/src/components/CardProduct.vue
@@ -1,60 +1,48 @@
 <template>
-  <v-card class="mx-auto" max-width="300" @click="tapped()">
-    <v-img height="150px" :src="product.imgURL"></v-img>
+  <div class="d-lg-flex d-inline">
+    <v-card class="mx-auto my-3" max-width="300" @click="tapped()" v-for="product in products" :key="product._id">
+      <v-img height="150px" src="../assets/logo.png"></v-img>
 
-    <v-card-title class="text-uppercase">{{product.nomArt}}</v-card-title>
+      <v-card-title class="text-uppercase">{{product.nomArt}}</v-card-title>
 
-    <v-card-subtitle class="pb-0">
-      <v-chip
-        class="ma-2"
-        x-small
-        color="primary"
-        v-for="category in categorys"
-        :key="category"
-      >{{category}}</v-chip>
-    </v-card-subtitle>
+      <v-card-subtitle class="pb-0">
+        <v-chip
+          class="ma-2"
+          x-small
+          color="primary"
+        >{{product.ctg}}</v-chip>
+      </v-card-subtitle>
 
-    <v-card-text class="text--primary">
-      <div>{{product.descripcion | cutText}}</div>
-      <p class="pt-3 product-price font-weight-bold">{{product.precio | prefixMoney}}</p>
-    </v-card-text>
+      <v-card-text class="text--primary">
+        <div>{{product.descripcion | cutText}}</div>
+        <p class="pt-3 product-price font-weight-bold">{{product.precio | prefixMoney}}</p>
+      </v-card-text>
 
-  </v-card>
+    </v-card>
+  </div>
 </template>
 
 <script>
-const Product = {
-  nomArt: "Casa de campo",
-  descripcion:
-    "description product Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  imgURL: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
-  precio: 130000,
-  fecSub: "27/10/2019",
-  lugVen: "Santo Domingo",
-  cnd: "Usado",
-  ctg: "casa, alquiler, hospedaje",
-  scn: "Imobiliario"
-};
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "CardProduct",
-  props: {
-    product: {
-      type: Object,
-      default() {
-        return { ...Product };
-      }
-    }
+  async created () {
+    await this.loadProducts();
   },
   methods: {
+    ...mapActions(['getProductsAction']),
     tapped() {
       console.log("Hola")
+    },
+    async loadProducts() {
+      console.log('wait');
+      await this.getProductsAction()
     }
   },
   computed: {
-    categorys() {
-      return this.product.ctg.split(",").map(category => category.trim());
-    }
+    ...mapState(['products'])
+ 
   },
   filters: {
     cutText(value) {
