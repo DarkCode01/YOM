74c31881c21cb3a12ca09a0db80f4bd29b2bd4ea
Rafael Mejia <devrafx@gmail.com>
2019-11-04 14:24:57 -0400

    Store creado y axios agregado para realizar peticiones.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,50 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as axios from 'axios';
+
+/**
+ * Realiza una eticion http get al servidor
+*/
+async function getProducts() {
+    const response = await axios.get('http://localhost:1234')
+    
+    return response.data;
+  }
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+/**
+ * Object state aislado para mejor manejo
+ */
+const state = {
+  products: []
+};
+
+/**
+ * Object mutations aislado para mejor manejo
+ */
+const mutations = {
+  getProducts(state, products){
+    state.products = [...products];
   }
+};
+
+const actions = {
+  async getProductsAction({ commit }){
+    console.log("Lol")
+    const products = await getProducts();
+    console.log(products);
+    commit('getProducts', products);
+
+  }
+};
+
+const getters = {};
+
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
 })
